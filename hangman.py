5a1084d58549b07d9c5702aa788ae49e3b7ee3c7
Philip Banta <Pbants@gmail.com>
2015-02-16 13:24:58 -0700

    Updated Code, adding dict file

--- hangman.py
+++ hangman.py
@@ -1,47 +1,146 @@
 import random
 from numpy import matrix
+import re
 
 print("Welcome to Hangman and goodluck!")
 
-#read random line from a file containing words
-word = random.choice(list(open("Dict.txt")))
+#Create a list of words from a file, then choose a random word from the list 
+#and strip spaces, new lines etc
+words = list(open("Dict.txt"))
+word = random.choice(words).rstrip()
+print word #for testing purposes
 
-for i in len(word):
-	answer[i] = "_" #and in the same amount of blanks as the word 
+answer = []
+for i in word:
+	answer.append('_')
 
-#use a matrix to represent the hangman
-man = matrix([['_','_','_'],['_','_','_'],['_','_','_']])
-full_man = matrix([["\\",'O','/'],['_','|','_'],['/','_','\\']])
+#use a list to represent the hangman
+HANGMAN = (
+"""
+ ------
+ |    |
+ |
+ |
+ |
+ |
+ |
+ |
+ |
+----------
+""",
+"""
+ ------
+ |    |
+ |    O
+ |
+ |
+ |
+ |
+ |
+ |
+----------
+""",
+"""
+ ------
+ |    |
+ |    O
+ |   -+-
+ | 
+ |   
+ |   
+ |   
+ |   
+----------
+""",
+"""
+ ------
+ |    |
+ |    O
+ |  /-+-
+ |   
+ |   
+ |   
+ |   
+ |   
+----------
+""",
+"""
+ ------
+ |    |
+ |    O
+ |  /-+-/
+ |   
+ |   
+ |   
+ |   
+ |   
+----------
+""",
+"""
+ ------
+ |    |
+ |    O
+ |  /-+-/
+ |    |
+ |   
+ |   
+ |   
+ |   
+----------
+""",
+"""
+ ------
+ |    |
+ |    O
+ |  /-+-/
+ |    |
+ |    |
+ |   | 
+ |   | 
+ |   
+----------
+""",
+"""
+ ------
+ |    |
+ |    O
+ |  /-+-/
+ |    |
+ |    |
+ |   | |
+ |   | |
+ |  
+----------
+""")
 
+prev_guess = [] #list of previous guesses
+incorrect = 0
+correct = 0
 
-prev_guess = {} #list of previous guesses
+while(incorrect != 8 and correct < len(word)):
+	guess = raw_input("Guess a Letter: ")
 
-while(guesses<7):
-	guess = raw_input("Guess a Letter")
-
-	if(guess is contained in word):
+	if(guess in word):
 		print("That letter is part of the word!")
-
-		#add the letter to the answer list in the same location as the original word
-		local = word.index(guess)
-		answer[local] = guess
-
+		for i in re.finditer(guess, word):
+			local = i.start()
+			#add the letter to the answer list in the same location as the original word
+			#local = word.index(guess)
+			answer[local] = guess
+		print HANGMAN[incorrect]
 		print ("%s") % answer
+		correct = correct + 1
 
-	elif(guess is not contained in word):
-		print("That letter is not part of the word!")
+	elif(not(guess in word)):
 		prev_guess.append(guess)
-		for i in man:
-			if man[i] = full_man[i]
-				continue
-			else if man[i] != full_man[i]:
-				man[i] = full_man[i]
+		print("That letter is not part of the word!")
 		print ("Used Letters %s") % prev_guess
 		print ("***" * 10)
-		print man
+		print HANGMAN[incorrect]
 		print ("%s") % answer
+		incorrect = incorrect + 1
+
 	else:
 		print("Please enter a letter from the alphabet!")
 
-	guesses++
-
+	
