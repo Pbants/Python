afe34c1860314b95a9535cdc4d57994e384b9bb9
Philip Banta <pbants@gmail.com>
2015-06-09 13:38:59 -0400

    Removed pass hash

--- Reddit.py
+++ Reddit.py
@@ -4,9 +4,10 @@ from pprint import *
 import time
 #Allows for colored text to be printed in terminal
 from termcolor import colored, cprint
+import base64
 
 #User agent name in accordance with Reddit API
-user_agent = "User-Agent: Crawl /r/gardening test v1.2 by /u/comtb"
+user_agent = "User-Agent: Crawl /r/gardening test v1.3 by /u/comtb"
 
 r = praw.Reddit(user_agent=user_agent)
 
@@ -14,14 +15,14 @@ r = praw.Reddit(user_agent=user_agent)
 r.login(username='comtb')
 
 #input favorite reddit subs here
-favorites = ['gardening']
+favorites = ['Gardening','WorldNews']
 
 #loop until keyboard inturrupt ctrl-c
 while True:
-	#Go through each subreddit in favorites and get the top 5 submissions under hot, can change to get_new
+	#Go through each subreddit in favorites and get the top 5 submissions under hot, can switch betwen get_new and get_hot
 	for sub in favorites:
 		subreddit = r.get_subreddit(sub)
-		for submission in subreddit.get_hot(limit = 5):
+		for submission in subreddit.get_new(limit = 5):
 			cprint (subreddit, 'red')
 			print (submission.title)
 			cprint (submission.url, 'magenta')
