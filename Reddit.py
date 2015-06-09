558400ac92c631e315b372447264137d903e3c31
Philip Banta <Pbants@gmail.com>
2015-06-09 11:38:43 -0400

    Updated comments and added Favorites variable

--- Reddit.py
+++ Reddit.py
@@ -2,13 +2,25 @@ import praw
 from pprint import *
 import time
 
+#User agent name in accordance with Reddit API
 user_agent = "User-Agent: Crawl /r/gardening test v1 by /u/comtb"
+
 r = praw.Reddit(user_agent=user_agent)
+
+#set username, can also include password
 r.login(username='comtb')
-already_done = []
-subreddit = r.get_subreddit('gardening')
+
+#input favorite reddit subs here
+favorites = ['gardening', 'mtb', 'Diablo3']
+
+#loop until keyboard inturrupt
 while True:
-	for submission in subreddit.get_hot(limit = 5):
-		print submission.title
-		print submission.url
+	#Go through each subreddit in favorites and get the top 5 submissions under hot, can change to new
+	for sub in favorites:
+		subreddit = r.get_subreddit(sub)
+		for submission in subreddit.get_hot(limit = 5):
+			print subreddit
+			print submission.title
+			print submission.url
+	#wait time in accordance with Reddit API
 	time.sleep(1800)
