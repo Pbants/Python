2ddcc7498c91cf561c025dfac867fd925020ef43
Philip Banta <Pbants@gmail.com>
2015-06-09 12:31:31 -0400

    Color printing in terminal, updated comments

--- Reddit.py
+++ Reddit.py
@@ -1,9 +1,12 @@
+#Import PRAW (for reddit)
 import praw
 from pprint import *
 import time
+#Allows for colored text to be printed in terminal
+from termcolor import colored, cprint
 
 #User agent name in accordance with Reddit API
-user_agent = "User-Agent: Crawl /r/gardening test v1 by /u/comtb"
+user_agent = "User-Agent: Crawl /r/gardening test v1.2 by /u/comtb"
 
 r = praw.Reddit(user_agent=user_agent)
 
@@ -11,16 +14,16 @@ r = praw.Reddit(user_agent=user_agent)
 r.login(username='comtb')
 
 #input favorite reddit subs here
-favorites = ['gardening', 'mtb', 'Diablo3']
+favorites = ['gardening']
 
-#loop until keyboard inturrupt
+#loop until keyboard inturrupt ctrl-c
 while True:
-	#Go through each subreddit in favorites and get the top 5 submissions under hot, can change to new
+	#Go through each subreddit in favorites and get the top 5 submissions under hot, can change to get_new
 	for sub in favorites:
 		subreddit = r.get_subreddit(sub)
 		for submission in subreddit.get_hot(limit = 5):
-			print subreddit
-			print submission.title
-			print submission.url
+			cprint (subreddit, 'red')
+			print (submission.title)
+			cprint (submission.url, 'magenta')
 	#wait time in accordance with Reddit API
 	time.sleep(1800)
